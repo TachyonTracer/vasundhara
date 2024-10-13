7446b35e7836df5a093c34763beec2ed74dfc103
Priyansh Pankhaniya <PriyanshPankhaniya@gmail.com>
2024-10-13 14:55:06 +0530

    Auth Intergration

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -2,10 +2,16 @@ import React from 'react'
 
 export default function page() {
   return (
-    <div>
-      <h1>
-        no way
-      </h1>
+    <div id="loginModal" className="modal">
+        <div className="modal-content">
+            <span className="close">&times;</span>
+            <h2>Login</h2>
+            <form id="loginForm">
+                <input type="text" id="loginUsername" placeholder="Username" required />
+                <input type="password" id="loginPassword" placeholder="Password" required />
+                <button type="submit">Login</button>
+            </form>
+        </div>
     </div>
   )
 }
